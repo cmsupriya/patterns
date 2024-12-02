da1034453b0a83e4297eb5a035e11542ed56a66b
cmsupriya <cm.supriyasahoo@gmailcom>
2024-12-02 21:08:48 +0530

    Singleton

--- src/main/java/com/upgrad/patterns/Config/RestServiceGenerator.java
+++ src/main/java/com/upgrad/patterns/Config/RestServiceGenerator.java
@@ -12,25 +12,31 @@ public class RestServiceGenerator {
 
     private static RestTemplate restTemplate;
 
-    private RestServiceGenerator()
-    {
+    private RestServiceGenerator() {
     }
 
-    public static RestTemplate GetInstance()
-    {
+    public static RestTemplate GetInstance() {
         // return restTemplate object if initialized already
-    	
+        if (restTemplate != null) {
+            return restTemplate;
+        }
 
         // Initialize restTemplate. This is executed only once.
-        restTemplate = new RestTemplateBuilder()
-                .interceptors((request, body, execution) -> {
-                    logger.info(String.format("Calling %s %s", request.getMethod(), request.getURI()));
-                    ClientHttpResponse clientHttpResponse = execution.execute(request, body);
-                    logger.info(String.format("Call completed %s %s responded with %s", request.getMethod(), request.getURI(), clientHttpResponse.getStatusCode()));
-                    return clientHttpResponse;
-                    })
-                .build();
-        
-        //return restTemplate object
+        synchronized (RestTemplate.class) {
+            if (restTemplate == null) {
+                restTemplate = new RestTemplateBuilder()
+                        .interceptors((request, body, execution) -> {
+                            logger.info(String.format("Calling %s %s", request.getMethod(), request.getURI()));
+                            ClientHttpResponse clientHttpResponse = execution.execute(request, body);
+                            logger.info(String.format("Call completed %s %s responded with %s", request.getMethod(),
+                                    request.getURI(), clientHttpResponse.getStatusCode()));
+                            return clientHttpResponse;
+                        })
+                        .build();
+            }
+        }
+
+        // return restTemplate object
+        return restTemplate;
     }
 }
